ace9aa7400724ab0bad37f760b89c443fb22365b
Nicole <janenieggcs@gmail.com>
2023-05-31 19:08:59 -0500

    Se agregó el modelo CUSTOMER y DETAIL

--- DDAEA_Lab_S11/Models/Customer.cs
+++ DDAEA_Lab_S11/Models/Customer.cs
@@ -7,5 +7,9 @@ namespace DDAEA_Lab_S11.Models
 {
     public class Customer
     {
+        public int CustomerID { get; set; }
+        public string CustomerFirstName { get; set; }
+        public string CustomerLastName { get; set; }
+        List<Invoice> Invoices { get; set; }
     }
 }
