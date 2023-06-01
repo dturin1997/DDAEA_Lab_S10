57ce00321986f9d6b79f201b5bf21f2a00ae8a7c
Dario Turin <dturin1997@gmail.com>
2023-05-31 19:10:14 -0500

    Se agregó Modelo Product e Invoice

--- DDAEA_Lab_S11/Models/Invoice.cs
+++ DDAEA_Lab_S11/Models/Invoice.cs
@@ -7,5 +7,11 @@ namespace DDAEA_Lab_S11.Models
 {
     public class Invoice
     {
+        public int InvoiceID { get; set; }
+        public DateTime Date { get; set; }
+        List<Detail> Details { get; set; }
+        public int CustomerID { get; set; }
+        public Customer Customer { get; set; }
+
     }
 }
--- DDAEA_Lab_S11/Models/Product.cs
+++ DDAEA_Lab_S11/Models/Product.cs
@@ -7,5 +7,10 @@ namespace DDAEA_Lab_S11.Models
 {
     public class Product
     {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Stock { get; set; }
+
     }
 }
